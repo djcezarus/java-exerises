93c9bbbd2812d8c62a173e0ea47931739db4e167
djcezarus <djcezarus@yahoo.com>
2022-09-28 11:43:19 +0100

    java exersises

---
+++ src/com/javacode/Main.java
@@ -0,0 +1,13 @@
+package com.javacode;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("welcome");
+        System.out.println("Java is fun");
+        System.out.print("Is it easy?");
+        System.out.print("I dont think so ... \n");
+        System.out.print("Some thing...");
+
+    }
+}
---
+++ src/com/qa/java/oop/Main.java
@@ -0,0 +1,4 @@
+package com.qa.java.oop;
+
+public class Main {
+}
